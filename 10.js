6b33a1c1abafe73832d08dcd423b5785fe5d1f37
krrakshit <rkrkr365@gmail.com>
2024-08-01 08:57:45 +0530

    Create 10.js

---
+++ 10.js
@@ -0,0 +1,10 @@
+
+let boy1 = "pramod"
+let boy2 = "nikhil"
+
+let sentence = `${boy1} is a friend of ${boy2}`
+console.log(sentence)
+
+// escape sequence character
+let fruit = "bana\"na"
+console.log(fruit)
