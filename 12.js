0c35534aa01fe5102168e0455e994da77908784b
krrakshit <rkrkr365@gmail.com>
2024-08-01 08:57:48 +0530

    Create 12.js

---
+++ 12.js
@@ -0,0 +1,13 @@
+let a = [91,82,73,64,false,"not present"]
+console.log(a)
+// a[1] = 89
+// console.log(a[0])
+// console.log(a[1])
+// console.log(a[2])
+// console.log(a[3])
+// console.log(a[4])
+// console.log(a[5])
+// console.log(a.length) 
+for(let i = 0;i<a.length;i++){
+console.log(a[i])
+}
