328166ce3dc0e503a52a435e379452f6c97ba1d4
krrakshit <rkrkr365@gmail.com>
2024-08-01 08:57:56 +0530

    Create 14.js

---
+++ 14.js
@@ -0,0 +1,8 @@
+let num = [1,2,3,4,5,6,7,8,9]
+// console.log(num.length)
+// delete num[0]
+// console.log(num.length)
+// console.log(num)
+let num2 = [11,12,13,14,15,16,17,18,19]
+let na = num.concat(num2)
+console.log(na)
