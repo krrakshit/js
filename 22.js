a9649d16053b49391aa37b3e61ffac0719541e9e
krrakshit <rkrkr365@gmail.com>
2024-08-04 23:50:01 +0530

    .

--- 22.js
+++ 22.js
@@ -1,2 +1,30 @@
  // snake water  gun question 
- 
+ let user = prompt("Enter S W or G")
+ let cpuI = Math.floor(Math.random()*3);
+let cpu =["S","W","G"][cpuI]
+
+const match = (cpu, user)=>{
+if(cpu === user){
+    return "Nobody won match tied"
+}
+else if(cpu ==="S" && user ==="W"){
+    return "cpu"
+}
+else if(cpu ==="S" && user ==="G"){
+    return "user"
+}
+else if(cpu ==="G" && user ==="W"){
+    return "user"
+}
+else if(cpu ==="G" && user ==="S"){
+    return "cpu"
+}
+else if(cpu ==="W" && user ==="G"){
+    return "cpu"
+}
+else if(cpu ==="W" && user ==="S"){
+    return "user"
+}
+}
+let result = match(cpu, user)
+document.write('CPU:${cpu} <br> user:${user} <br> The winner is : ${result}')
