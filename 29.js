be87b9b37c364c05485ccb3737f1f13c00bc2276
krrakshit <rkrkr365@gmail.com>
2024-08-06 23:32:23 +0530

    Create 29.js

---
+++ 29.js
@@ -0,0 +1,12 @@
+// first.insertAdjacentHTML('beforeend',<div class="test">beforeend</div>);
+//first.remove()
+
+first.className = "text-black red"
+first.classList
+DOMTokenList(2) ['text-black', 'red', value: 'text-black red']
+first.classList.remove("red")
+undefined
+first.classList.add("red")
+undefined
+first.classList.toggle("red")
+false
