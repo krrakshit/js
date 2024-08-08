55e3d3dc1437755bb52daad82bd1587ffd39ee8c
krrakshit <rkrkr365@gmail.com>
2024-08-08 23:02:52 +0530

    Create 41.js

---
+++ 41.js
@@ -0,0 +1,36 @@
+setTimeout(()=>{
+    console.log("turning on wifi please wait")
+},1000)
+
+try{
+
+    setTimeout(()=>{
+        try{
+
+        
+        console.log(rahul)
+        } catch(error){
+            
+        }
+    },100)
+    
+}
+catch(err){
+    console.log("hehe"+err)
+}
+
+setTimeout(()=>{
+    console.log("fetching user name and password")
+    
+},2000)
+
+setTimeout(()=>{
+    console.log("connecting elses software")
+},3000)
+
+
+setTimeout(()=>{
+    console.log("remote desktop connection enabbled")
+},4000)
+
+
