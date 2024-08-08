48591aef796b5eec99b19264eaa42dff7bde79ba
krrakshit <rkrkr365@gmail.com>
2024-08-08 23:02:45 +0530

    Create 43.js

---
+++ 43.js
@@ -0,0 +1,19 @@
+const f = ()=>{
+
+
+
+
+try {
+    let a = 0
+   // console.log(p)
+    console.log("program ran sucessfully")
+    return
+} catch (err) {
+    console.log("this is an error")
+}
+
+finally{
+    console.log("code scripted ")
+}
+}
+f()
