90ba3708476abcd17b6d720ceacef36ccca551d8
krrakshit <rkrkr365@gmail.com>
2024-08-25 22:50:51 +0530

    Create 58.js

---
+++ 58.js
@@ -0,0 +1,16 @@
+message = "Gd global"
+function hello1(){
+   let message = "hey there"
+    {
+
+    let message = "good noon"
+    console.log("hello1" + message)
+    }
+    console.log(message)
+    let c = function hello2(){
+        console.log("it is c")
+    }
+    return c
+}
+hello1()
+c() 
